ff2e7239af04e03e0d03cddb10c77fe9c9bf00d5
John Mark <john.m.goeke@vanderbilt.edu>
2020-07-12 07:35:47 -0500

    fixed modal errors

--- frontend/wc.js
+++ frontend/wc.js
@@ -67,7 +67,7 @@ fetch("http://127.0.0.1:5000/test", requestOptions)
     var modal = document.getElementById("myModal");
     var modspan = document.getElementsByClassName("close")[0];
     var result_text = document.getElementById("result_text");
-        result_text.innerHTML = "Your results: " + result;
+        result_text.textContent = result;
     modspan.onclick = function() {
       modal.style.display = "none";
     }
